a2cbb4e42ea9a48e34c3a83b201118cf71453c9e
Mathieu Dutour <mathieu@dutour.me>
2020-11-10 12:18:53 +0100

    Prereleases (#37)
    Co-authored-by: Moritz Schmitz von Hülst <moritz@hauptstadtoffice.com>

--- README.md
+++ README.md
@@ -14,31 +14,48 @@ jobs:
   build:
     runs-on: ubuntu-latest
     steps:
-      - uses: actions/checkout@master
-        with:
-          # Fetches entire history, so we can analyze commits since last tag
-          fetch-depth: 0
+      - uses: actions/checkout@v2
       - name: Bump version and push tag
-        uses: mathieudutour/github-tag-action@v4.6
+        id: tag_version
+        uses: mathieudutour/github-tag-action@v5
         with:
           github_token: ${{ secrets.GITHUB_TOKEN }}
+      - name: Create a GitHub release
+        uses: actions/create-release@v1
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+        with:
+          tag_name: ${{ steps.tag_version.outputs.new_tag }}
+          release_name: Release ${{ steps.tag_version.outputs.new_tag }}
+          body: ${{ steps.tag_version.outputs.changelog }}
 ```
 
-### Inputs
+### 📥 Inputs
 
 - **github_token** _(required)_ - Required for permission to tag the repo. Usually `${{ secrets.GITHUB_TOKEN }}`.
-- **default_bump** _(optional)_ - Which type of bump to use when [none is explicitly provided](#bumping) (default: `patch`). You can also set `false` to avoid generating a new tag when none is explicitly provided.
-- **tag_prefix** _(optional)_ - A prefix to the tag name (default: `v`).
-- **custom_tag** _(optional)_ - Custom tag name. If specified, it overrides bump settings. 
-- **release_branches** _(optional)_ - Comma separated list of branches (bash reg exp accepted) that will generate the release tags. Other branches and pull-requests generate versions postfixed with the commit hash and do not generate any tag. Examples: `master` or `.*` or `release.*,hotfix.*,master`... (default: `master`).
-- **create_annotated_tag** _(optional)_ - Boolean to create an annotated rather than a lightweight one (default: `false`).
-- **dry_run** _(optional)_ - Do not perform taging, just calculate next version and changelog, then exit
 
-### Outputs
+#### Filter branches
+
+- **release_branches** _(optional)_ - Comma separated list of branches (JavaScript regular expression accepted) that will generate the release tags. Other branches and pull-requests generate versions postfixed with the commit hash and do not generate any tag. Examples: `master` or `.*` or `release.*,hotfix.*,master`... (default: `master`).
+- **pre_release_branches** _(optional)_ - Comma separated list of branches (JavaScript regular expression accepted) that will generate pre-release tags.
+
+#### Customize the tag
+
+- **default_bump** _(optional)_ - Which type of bump to use when [none is explicitly provided](#bumping) (default: `patch`). You can also set `false` to avoid generating a new tag when none is explicitly provided.
+- **custom_tag** _(optional)_ - Custom tag name. If specified, it overrides bump settings.
+- **create_annotated_tag** _(optional)_ - Boolean to create an annotated rather than a lightweight one (default: `false`).
+- **tag_prefix** _(optional)_ - A prefix to the tag name (default: `v`).
+- **append_to_pre_release_tag** _(optional)_ - A suffix to the pre-release tag name (default: `<branch>`).
+
+#### Debugging
+
+- **dry_run** _(optional)_ - Do not perform tagging, just calculate next version and changelog, then exit
+
+### 📤 Outputs
 
 - **new_tag** - The value of the newly created tag. Note that if there hasn't been any new commit, this will be `undefined`.
 - **new_version** - The value of the newly created tag without the prefix. Note that if there hasn't been any new commit, this will be `undefined`.
-- **previous_tag** - The value of the previous tag (or `0.0.0` if none).
+- **previous_tag** - The value of the previous tag (or `0.0.0` if none). Note that if `custom_tag` is set, this will be `undefined`.
 - **changelog** - The [conventional changelog](https://github.com/conventional-changelog/conventional-changelog) since the previous tag.
 
 > **_Note:_** This action creates a [lightweight tag](https://developer.github.com/v3/git/refs/#create-a-reference) by default.
--- src/main.ts
+++ src/main.ts
@@ -1,144 +1,121 @@
 import * as core from "@actions/core";
-import { exec as _exec } from "@actions/exec";
-import { context, GitHub } from "@actions/github";
-import { inc, ReleaseType } from "semver";
+import { inc, parse, ReleaseType, valid, gte, SemVer } from "semver";
 import { analyzeCommits } from "@semantic-release/commit-analyzer";
 import { generateNotes } from "@semantic-release/release-notes-generator";
-
-const HASH_SEPARATOR = "|commit-hash:";
-const SEPARATOR = "==============================================";
-
-async function exec(command: string) {
-  let stdout = "";
-  let stderr = "";
-
-  try {
-    const options = {
-      listeners: {
-        stdout: (data: Buffer) => {
-          stdout += data.toString();
-        },
-        stderr: (data: Buffer) => {
-          stderr += data.toString();
-        },
-      },
-    };
-
-    const code = await _exec(command, undefined, options);
-
-    return {
-      code,
-      stdout,
-      stderr,
-    };
-  } catch (err) {
-    return {
-      code: 1,
-      stdout,
-      stderr,
-      error: err,
-    };
-  }
-}
+import {
+  getValidTags,
+  getCommits,
+  getBranchFromRef,
+  getLatestTag,
+  createTag,
+  getLatestPrereleaseTag,
+} from "./utils";
 
 async function run() {
   try {
     const defaultBump = core.getInput("default_bump") as ReleaseType | "false";
     const tagPrefix = core.getInput("tag_prefix");
-    const customTag = core.getInput("custom_tag")
+    const customTag = core.getInput("custom_tag");
     const releaseBranches = core.getInput("release_branches");
-    const createAnnotatedTag = core.getInput("create_annotated_tag");
+    const preReleaseBranches = core.getInput("pre_release_branches");
+    const appendToPreReleaseTag = core.getInput("append_to_pre_release_tag");
+    const createAnnotatedTag = !!core.getInput("create_annotated_tag");
     const dryRun = core.getInput("dry_run");
 
     const { GITHUB_REF, GITHUB_SHA } = process.env;
 
     if (!GITHUB_REF) {
-      core.setFailed("Missing GITHUB_REF");
+      core.setFailed("Missing GITHUB_REF.");
       return;
     }
 
     if (!GITHUB_SHA) {
-      core.setFailed("Missing GITHUB_SHA");
+      core.setFailed("Missing GITHUB_SHA.");
       return;
     }
 
-    const preRelease = releaseBranches
+    const currentBranch = getBranchFromRef(GITHUB_REF);
+    const isReleaseBranch = releaseBranches
       .split(",")
-      .every((branch) => !GITHUB_REF.replace("refs/heads/", "").match(branch));
+      .some((branch) => currentBranch.match(branch));
+    const isPreReleaseBranch = preReleaseBranches
+      .split(",")
+      .some((branch) => currentBranch.match(branch));
+    const isPrerelease = !isReleaseBranch && isPreReleaseBranch;
 
-    await exec("git fetch --tags");
-
-    const hasTag = !!(await exec("git tag")).stdout.trim();
-    let tag = "";
-    let logs = "";
-
-    if (hasTag) {
-      const previousTagSha = (
-        await exec("git rev-list --tags --topo-order --max-count=1")
-      ).stdout.trim();
-      tag = (await exec(`git describe --tags ${previousTagSha}`)).stdout.trim();
-      logs = (
-        await exec(
-          `git log ${tag}..HEAD --pretty=format:'%s%n%b${HASH_SEPARATOR}%h${SEPARATOR}' --abbrev-commit`
-        )
-      ).stdout.trim();
-
-      core.debug(`Setting previous_tag to: ${tag}`);
-      core.setOutput("previous_tag", tag);
-
-      if (previousTagSha === GITHUB_SHA) {
-        core.debug("No new commits since previous tag. Skipping...");
-        return;
-      }
-    } else {
-      tag = "0.0.0";
-      logs = (
-        await exec(
-          `git log --pretty=format:'%s%n%b${HASH_SEPARATOR}%h${SEPARATOR}' --abbrev-commit`
-        )
-      ).stdout.trim();
-      core.setOutput("previous_tag", tag);
-    }
-
-    // for some reason the commits start and end with a `'` on the CI so we ignore it
-    const commits = logs
-      .split(SEPARATOR)
-      .map((x) => {
-        const data = x.trim().replace(/^'\n'/g, "").replace(/^'/g, "");
-        if (!data) {
-          return {};
-        }
-        const [message, hash] = data.split(HASH_SEPARATOR);
-        return {
-          message: message.trim(),
-          hash: hash.trim(),
-        };
-      })
-      .filter((x) => !!x.message);
-    const bump = await analyzeCommits(
-      {},
-      { commits, logger: { log: console.info.bind(console) } }
+    const validTags = await getValidTags();
+    const latestTag = getLatestTag(validTags);
+    const latestPrereleaseTag = getLatestPrereleaseTag(
+      validTags,
+      currentBranch
     );
 
-    if (!bump && defaultBump === "false") {
-      core.debug("No commit specifies the version bump. Skipping...");
-      return;
-    }
+    const commits = await getCommits(latestTag.commit.sha);
 
-    let newVersion = "";
-    if (!customTag) {
-      newVersion = `${inc(tag, bump || defaultBump)}${
-        preRelease ? `-${GITHUB_SHA.slice(0, 7)}` : ""
-      }`;
-    } else {
+    let newVersion: string;
+
+    if (customTag) {
       newVersion = customTag;
+    } else {
+      let previousTag: SemVer | null;
+      if (!latestPrereleaseTag) {
+        previousTag = parse(latestTag.name);
+      } else {
+        previousTag = parse(
+          gte(latestTag.name, latestPrereleaseTag.name)
+            ? latestTag.name
+            : latestPrereleaseTag.name
+        );
+      }
+
+      if (!previousTag) {
+        core.setFailed("Could not parse previous tag.");
+        return;
+      }
+
+      core.info(`Previous tag was ${previousTag}.`);
+      core.setOutput("previous_tag", previousTag.version);
+
+      const bump = await analyzeCommits(
+        {},
+        { commits, logger: { log: console.info.bind(console) } }
+      );
+
+      if (!bump && defaultBump === "false") {
+        core.debug(
+          "No commit specifies the version bump. Skipping the tag creation."
+        );
+        return;
+      }
+
+      const releaseType: ReleaseType = isPrerelease
+        ? "prerelease"
+        : bump || defaultBump;
+      const incrementedVersion = inc(
+        previousTag,
+        releaseType,
+        appendToPreReleaseTag ? appendToPreReleaseTag : currentBranch
+      );
+
+      if (!incrementedVersion) {
+        core.setFailed("Could not increment version.");
+        return;
+      }
+
+      if (!valid(incrementedVersion)) {
+        core.setFailed(`${incrementedVersion} is not a valid semver.`);
+        return;
+      }
+
+      newVersion = incrementedVersion;
     }
-    const newTag = `${tagPrefix}${newVersion}`;
 
+    core.info(`New version is ${newVersion}.`);
     core.setOutput("new_version", newVersion);
-    core.setOutput("new_tag", newTag);
 
-    core.debug(`New tag: ${newTag}`);
+    const newTag = `${tagPrefix}${newVersion}`;
+    core.info(`New tag after applying prefix is ${newTag}.`);
+    core.setOutput("new_tag", newTag);
 
     const changelog = await generateNotes(
       {},
@@ -148,26 +125,22 @@ async function run() {
         options: {
           repositoryUrl: `https://github.com/${process.env.GITHUB_REPOSITORY}`,
         },
-        lastRelease: { gitTag: tag },
+        lastRelease: { gitTag: latestTag.name },
         nextRelease: { gitTag: newTag, version: newVersion },
       }
     );
-
+    core.info(`Changelog is ${changelog}.`);
     core.setOutput("changelog", changelog);
 
-    if (preRelease) {
-      core.debug(
-        "This branch is not a release branch. Skipping the tag creation."
+    if (!isReleaseBranch && !isPreReleaseBranch) {
+      core.info(
+        "This branch is neither a release nor a pre-release branch. Skipping the tag creation."
       );
       return;
     }
 
-    const tagAlreadyExists = !!(
-      await exec(`git tag -l "${newTag}"`)
-    ).stdout.trim();
-
-    if (tagAlreadyExists) {
-      core.debug("This tag already exists. Skipping the tag creation.");
+    if (validTags.map((tag) => tag.name).includes(newTag)) {
+      core.info("This tag already exists. Skipping the tag creation.");
       return;
     }
 
@@ -176,36 +149,7 @@ async function run() {
       return;
     }
 
-    const octokit = new GitHub(core.getInput("github_token"));
-
-    if (createAnnotatedTag === "true") {
-      core.debug(`Creating annotated tag`);
-
-      const tagCreateResponse = await octokit.git.createTag({
-        ...context.repo,
-        tag: newTag,
-        message: newTag,
-        object: GITHUB_SHA,
-        type: "commit",
-      });
-
-      core.debug(`Pushing annotated tag to the repo`);
-
-      await octokit.git.createRef({
-        ...context.repo,
-        ref: `refs/tags/${newTag}`,
-        sha: tagCreateResponse.data.sha,
-      });
-      return;
-    }
-
-    core.debug(`Pushing new tag to the repo`);
-
-    await octokit.git.createRef({
-      ...context.repo,
-      ref: `refs/tags/${newTag}`,
-      sha: GITHUB_SHA,
-    });
+    await createTag(newTag, createAnnotatedTag, GITHUB_SHA);
   } catch (error) {
     core.setFailed(error.message);
   }
