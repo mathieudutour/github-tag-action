ddafd831089b2f336b16d2661592a636e4adf69f
Mathieu Dutour <mathieu@dutour.me>
2021-04-01 15:30:58 +0200

    remove unused types

--- src/ts.ts
+++ src/ts.ts
@@ -3,32 +3,3 @@ export type Await<T> = T extends {
 }
   ? U
   : T;
-
-export type Nullable<T> = null | T;
-
-export type ReleaseRule = {
-  /**
-   * Commit type.
-   * Eg: feat, fix etc.
-   */
-  type: string;
-  /**
-   * Release types.
-   * Eg: major, minor, patch etc.
-   */
-  release: string;
-  scope?: string;
-};
-
-export type ChangelogRule = {
-  /**
-   * Commit type.
-   * Eg: feat, fix etc.
-   */
-  type: string;
-  /**
-   * Section in changelog to group commits by type.
-   * Eg: 'Bug Fix', 'Features' etc.
-   */
-  section?: string;
-};
