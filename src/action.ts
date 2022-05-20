c6ef5fb3597c68e982ae76e4f470d1461e2f46a5
E Einowski <eeinowski@cloudbolt.io>
2022-05-20 14:47:00 -0700

    allow PRs to be prereleases

--- src/action.ts
+++ src/action.ts
@@ -57,7 +57,7 @@ export default async function main() {
     .split(',')
     .some((branch) => currentBranch.match(branch));
   const isPullRequest = isPr(GITHUB_REF);
-  const isPrerelease = !isReleaseBranch && !isPullRequest && isPreReleaseBranch;
+  const isPrerelease = !isReleaseBranch && isPreReleaseBranch;
 
   // Sanitize identifier according to
   // https://semver.org/#backusnaur-form-grammar-for-valid-semver-versions
